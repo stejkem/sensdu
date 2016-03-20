3acd6bd39be97f42f56a8a76c53ce6a4b274aa39
stejkem <stejkem@gmail.com>
2016-03-20 18:36:24 +0200

    Packaging as war was added.

--- src/main/java/com/sensdu/SensduApplication.java
+++ src/main/java/com/sensdu/SensduApplication.java
@@ -2,11 +2,20 @@ package com.sensdu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SensduApplication {
+public class SensduApplication extends SpringBootServletInitializer {
+    private static Class<SensduApplication> applicationClass = SensduApplication.class;
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SensduApplication.class, args);
     }
 }
+
