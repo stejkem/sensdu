f5c2174841bfcb5283c0b6b778c555c1de42b337
stejkem <stejkem@gmail.com>
2016-04-07 08:41:38 +0300

    Gitignore, additional tests (commented), web-site address was added.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sensdu
 Sense-for-sense translation powered by Wikipedia.
+You could test the current version on:
+http://sensdu-stejkem.rhcloud.com
+
--- src/test/java/com/sensdu/model/SensduCoreTest.java
+++ src/test/java/com/sensdu/model/SensduCoreTest.java
@@ -2,15 +2,18 @@ package com.sensdu.model;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by stejkem on 13/03/2016.
- */
 public class SensduCoreTest {
 
     @org.junit.Test
     public void testGetTranslation() throws Exception {
         assertEquals("Myky", new SensduCore("Dumpling", "en2fi").getTranslation());
         assertEquals("Метрополітен", new SensduCore("Rapid transit", "en2uk").getTranslation());
+
+        //new test to extent the functionality. Added 07/04/2015.
+
+        //There is only 'Кріп пахучий" page on wikipedia.
+        //assertEquals("Dill", new SensduCore("Кріп", "uk2en").getTranslation());
+
 
     }
 }
