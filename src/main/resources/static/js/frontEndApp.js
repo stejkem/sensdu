0ddf5450c1a28e886261b9ff31d3636edd430d74
stejkem <stejkem@users.noreply.github.com>
2016-06-08 17:12:38 +0300

    Migrated from Maven to Gradle. Added Swap Languages button.

--- src/main/resources/static/js/frontEndApp.js
+++ src/main/resources/static/js/frontEndApp.js
@@ -1,6 +1,6 @@
-    var routerApp = angular.module('frontEndApp', ['ui.router' , 'ui.bootstrap']);
+    var frontEndApp = angular.module('frontEndApp', ['ui.router' , 'ui.bootstrap']);
 
-    routerApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    frontEndApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
         $urlRouterProvider.otherwise('/404');
         $stateProvider
@@ -63,30 +63,60 @@
             $scope.sensdu.translatedWordURL = null;
         }
 
-         $scope.changeLanguageInToGroup = function(toLanguageCode, toLanguage) {
-             $scope.formHelper.toLanguageCode = toLanguageCode;
-             $scope.formHelper.toLanguage = toLanguage;
-             $scope.sensdu.toLanguage = toLanguageCode;
-         }
+        $scope.swapLanguages = function() {
+            if(($scope.formHelper.fromLanguageCode == $scope.sensdu.fromLanguage) &&
+                ($scope.formHelper.toLanguageCode != $scope.sensdu.toLanguage))   {
+                 $scope.formHelper.toLanguage = $scope.formHelper.fromLanguage;
+                 $scope.formHelper.toLanguageCode = $scope.formHelper.fromLanguageCode;
+            }
+            if (($scope.formHelper.toLanguageCode == $scope.sensdu.toLanguage) &&
+                ($scope.formHelper.fromLanguageCode == $scope.sensdu.fromLanguage)) {
 
-         $scope.validateLanguagesInFromGroup = function() {
-             if($scope.sensdu.fromLanguage == $scope.sensdu.toLanguage) {
-                 if ($scope.sensdu.toLanguage == 'uk') {
-                    $scope.sensdu.toLanguage = 'en';
-                 } else {
-                    $scope.sensdu.toLanguage = 'uk';
-                 }
-             }
-         }
+                $scope.formHelper.fromLanguage = $scope.formHelper.toLanguage;
+                $scope.formHelper.fromLanguageCode = $scope.formHelper.toLanguageCode;
+            }
+            if (($scope.formHelper.fromLanguageCode == $scope.sensdu.fromLanguage) &&
+                ($scope.formHelper.toLanguageCode == $scope.sensdu.toLanguage)) {
+                var interMediate = $scope.formHelper.fromLanguage;
+                var interMediateCode = $scope.formHelper.fromLanguageCode;
+                $scope.formHelper.fromLanguage = $scope.formHelper.toLanguage;
+                $scope.formHelper.fromLanguageCode = $scope.formHelper.toLanguageCode;
+                $scope.formHelper.toLanguage = interMediate;
+                $scope.formHelper.toLanguageCode = interMediateCode;
+                $scope.sensdu.fromLanguage = $scope.formHelper.fromLanguageCode;
+                $scope.sensdu.toLanguage = $scope.formHelper.toLanguageCode;
 
-         $scope.validateLanguagesInToGroup = function() {
-             if($scope.sensdu.toLanguage == $scope.sensdu.fromLanguage) {
-                 if ($scope.sensdu.fromLanguage == 'en') {
-                    $scope.sensdu.fromLanguage = 'uk';
-                 } else {
-                    $scope.sensdu.fromLanguage = 'en';
-                 }
-             }
-         }
+            } else {
+                var interMediate = $scope.sensdu.fromLanguage;
+                $scope.sensdu.fromLanguage = $scope.sensdu.toLanguage;
+                $scope.sensdu.toLanguage = interMediate;
+            }
+        }
+
+        $scope.changeLanguageInToGroup = function(toLanguageCode, toLanguage) {
+            $scope.formHelper.toLanguageCode = toLanguageCode;
+            $scope.formHelper.toLanguage = toLanguage;
+            $scope.sensdu.toLanguage = toLanguageCode;
+        }
+
+        $scope.validateLanguagesInFromGroup = function() {
+            if($scope.sensdu.fromLanguage == $scope.sensdu.toLanguage) {
+                if ($scope.sensdu.toLanguage == 'uk') {
+                   $scope.sensdu.toLanguage = 'en';
+                } else {
+                   $scope.sensdu.toLanguage = 'uk';
+                }
+            }
+        }
+
+        $scope.validateLanguagesInToGroup = function() {
+            if($scope.sensdu.toLanguage == $scope.sensdu.fromLanguage) {
+                if ($scope.sensdu.fromLanguage == 'en') {
+                   $scope.sensdu.fromLanguage = 'uk';
+                } else {
+                   $scope.sensdu.fromLanguage = 'en';
+                }
+            }
+        }
 
     });
