6083f4eb0825667bcb625e5c9c79d944ac6cd3a5
stejkem <stejkem@gmail.com>
2016-07-13 00:57:51 +0300

    Bug in SpringApplication deployment on OpenShift corrected.

--- src/main/java/com/sensdu/Application.java
+++ src/main/java/com/sensdu/Application.java
@@ -2,13 +2,19 @@ package com.sensdu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    private static Class<Application> applicationClass = Application.class;
 
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
