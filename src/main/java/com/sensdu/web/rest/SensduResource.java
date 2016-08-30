26aa97025ed2cb7cbc3c22a06ae278e45a45a699
stejkem <stejkem@gmail.com>
2016-08-31 02:48:53 +0300

    Issue with cherry picking from commit c2e5849.

--- src/main/java/com/sensdu/web/rest/SensduResource.java
+++ src/main/java/com/sensdu/web/rest/SensduResource.java
@@ -1,15 +1,12 @@
 package com.sensdu.web.rest;
 
 import com.sensdu.core.SensduCore;
-import com.sensdu.domain.Query;
-import com.sensdu.repository.QueryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,8 +14,6 @@ import java.util.Map;
 public class SensduResource {
     private final Logger log = LoggerFactory.getLogger(SensduResource.class);
 
-    @Inject
-    private QueryRepository queryRepository;
 
     @RequestMapping(value="/resource")
     public ResponseEntity<Map<String, Object>> setData(@RequestBody SensduCore sensdu) {
