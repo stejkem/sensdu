25fdefbffa5ba37a44e646f2c3ee3937c144804a
stejkem <stejkem@gmail.com>
2016-08-31 02:44:51 +0300

    Issue with one-off word was resolved. (cherry picked from commit c2e5849)

--- src/main/java/com/sensdu/web/rest/SensduResource.java
+++ src/main/java/com/sensdu/web/rest/SensduResource.java
@@ -1,15 +1,24 @@
 package com.sensdu.web.rest;
 
 import com.sensdu.core.SensduCore;
+import com.sensdu.domain.Query;
+import com.sensdu.repository.QueryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class SensduResource {
+    private final Logger log = LoggerFactory.getLogger(SensduResource.class);
+
+    @Inject
+    private QueryRepository queryRepository;
 
     @RequestMapping(value="/resource")
     public ResponseEntity<Map<String, Object>> setData(@RequestBody SensduCore sensdu) {
@@ -28,6 +37,7 @@ public class SensduResource {
                     model.put("toLanguage", sensdu.getToLanguage());
                     model.put("fromLanguage", sensdu.getFromLanguage());
                     model.put("wordURL", sensdu.getSourceWordlURL());
+
                 } catch (NullPointerException e) {
                     if (!sensdu.getState().equals("ambiguousArticle") && sensdu.getSourceWordSearchSuggestion().size() != 0) {
                         model.put("wordSuggestion", sensdu.getSourceWordSearchSuggestion());
@@ -35,7 +45,9 @@ public class SensduResource {
                         model.put("fromLanguage", sensdu.getFromLanguage());
                         model.put("state", "ambiguousArticle");
                     } else {
-                        throw new NullPointerException();
+                        model.put("toLanguage", sensdu.getToLanguage());
+                        model.put("fromLanguage", sensdu.getFromLanguage());
+                        model.put("wordURL", sensdu.getSourceWordlURL());
                     }
                 }
             }
