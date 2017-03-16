4b4ca88f388f01a2ebab121f295d6a63503eaca4
stejkem <stejkem@gmail.com>
2017-03-16 22:32:14 +0200

    Addition of the redirect test.

--- src/test/java/com/sensdu/core/SensduTests.java
+++ src/test/java/com/sensdu/core/SensduTests.java
@@ -1,6 +1,5 @@
 package com.sensdu.core;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import com.sensdu.domain.TranslationQuery;
@@ -9,20 +8,27 @@ import static org.junit.Assert.assertEquals;
 
 public class SensduTests {
 
-    Sensdu sensdu;
-
-    @Before
-    public void init() throws Exception {
+    @Test
+    public void easyTranslationTest() throws Exception {
         TranslationQuery translationQuery = new TranslationQuery();
         translationQuery.setFromLanguage("en");
         translationQuery.setToLanguage("de");
         translationQuery.setWord("Big Bang");
 
-        sensdu = new Sensdu(translationQuery);
+        Sensdu sensdu = new Sensdu(translationQuery);
+
+        assertEquals("Urknall", sensdu.retrieveTranslationResponse().getTranslation());
     }
 
     @Test
-    public void testTranslation() throws Exception {
-        assertEquals("Urknall", sensdu.retrieveTranslationResponse().getTranslation());
+    public void translationWithRedirectTest() throws Exception {
+        TranslationQuery translationQuery = new TranslationQuery();
+        translationQuery.setFromLanguage("en");
+        translationQuery.setToLanguage("ja");
+        translationQuery.setWord("Landmass");
+
+        Sensdu sensdu = new Sensdu(translationQuery);
+
+        assertEquals("地", sensdu.retrieveTranslationResponse().getTranslation());
     }
 }
