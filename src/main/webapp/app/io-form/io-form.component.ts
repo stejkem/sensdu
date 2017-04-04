84981ca7a609d237e246efb7a0dff5c9f9f4907f
stejkem <stejkem@gmail.com>
2017-04-04 16:58:49 +0300

    Add a hull of a post request

--- src/main/webapp/app/io-form/io-form.component.ts
+++ src/main/webapp/app/io-form/io-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LANGUAGES } from "../app.constants";
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
 
 @Component({
   selector: 'app-io-form',
@@ -9,15 +10,27 @@ import { LANGUAGES } from "../app.constants";
 export class IoFormComponent implements OnInit {
   private languages: string[];
 
-  constructor() {
+  private loading: boolean;
+  private data: Object;
+
+  constructor(private http: Http) {
     this.languages = LANGUAGES;
   }
 
   onSubmit(form: any): void {
-    console.log('The value you\'ve submitted:', form);
+    console.log('The form you\'ve submitted:', form);
+    this.loading = true;
+
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    this.http.post("http://localhost:8080/api/translate", JSON.stringify(form), options)
+      .subscribe((response: Response) => {
+        this.data = response.json();
+        this.loading = false;
+      });
   }
 
   ngOnInit() {
   }
-
 }
