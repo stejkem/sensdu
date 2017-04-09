0251934b316ecf4162927bb7abc8b7b7b85e9948
stejkem <stejkem@gmail.com>
2017-04-09 18:23:38 +0300

    Change extraction mechanism for first sentence from article

--- src/main/java/com/sensdu/service/TranslationService.java
+++ src/main/java/com/sensdu/service/TranslationService.java
@@ -1,19 +1,20 @@
 package com.sensdu.service;
 
-import java.io.IOException;
 import java.util.LinkedHashMap;
 
-import com.sensdu.utility.JsoupRequestWrapper;
-import com.sensdu.utility.MediaWikiAPIRequestBuilder;
-import org.apache.commons.lang3.StringUtils;
+import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import net.minidev.json.JSONArray;
 
 import com.jayway.jsonpath.JsonPath;
+
 import com.sensdu.domain.TranslationQuery;
 import com.sensdu.domain.TranslationResponse;
-import net.minidev.json.JSONArray;
-import org.springframework.stereotype.Service;
+import com.sensdu.utility.JsoupRequestWrapper;
+import com.sensdu.utility.MediaWikiAPIRequestBuilder;
 
 /**
  * @author Stejkem
@@ -25,46 +26,63 @@ public class TranslationService {
 
     private TranslationQuery translationQuery;
 
-    public TranslationResponse retrieveTranslationResponse() {
-        String langAndLinksPath = String.format(".query.pages[0].langlinks[?(@.lang == '%1$s')]", translationQuery.getToLanguage());
-        String wordURLPath = ".query.pages[0].fullurl";
-        String wordWikiExtractPath = ".query.pages[0].extract";
-
-        String translationURI = MediaWikiAPIRequestBuilder.buildTranslationRequestURI(translationQuery.getWord(), translationQuery.getFromLanguage());
-        String APIResponse = tryToMakeWebRequest(translationURI);
-
-        LinkedHashMap langAndLink = (LinkedHashMap) (((JSONArray) JsonPath.parse(APIResponse).read(langAndLinksPath)).get(0));
-        String wordURL = ((JSONArray) JsonPath.parse(APIResponse).read(wordURLPath)).get(0).toString();
-        String wordWikiExtract = ((JSONArray) JsonPath.parse(APIResponse).read(wordWikiExtractPath)).get(0).toString();
-
-        TranslationResponse translationResponse = new TranslationResponse();
-        translationResponse.setFromLanguage(translationQuery.getFromLanguage());
-        translationResponse.setToLanguage(translationQuery.getToLanguage());
-        translationResponse.setWord(translationQuery.getWord());
-        translationResponse.setWordURL(wordURL);
-        translationResponse.setWordWikiExtract(StringUtils.substringBefore(wordWikiExtract, ".") + "...");
-        translationResponse.setTranslation((String) langAndLink.get("title"));
-        translationResponse.setTranslationURL((String) langAndLink.get("url"));
-
-        return translationResponse;
-    }
-
-    private String tryToMakeWebRequest(String translationURI) {
-        try {
-            return JsoupRequestWrapper.retrieveJson(translationURI);
-        } catch (IOException exception) {
-            String message = "Looks like the base language of the page was set incorrectly - " + exception.getMessage();
-            logger.error(message);
-
-            throw new RuntimeException(message);
-        }
+    public TranslationQuery getTranslationQuery() {
+        return translationQuery;
     }
 
     public void setTranslationQuery(TranslationQuery translationQuery) {
         this.translationQuery = translationQuery;
     }
 
-    public TranslationQuery getTranslationQuery() {
-        return translationQuery;
+    public TranslationResponse retrieveTranslationResponse() {
+        String langAndLinksPath = String.format(".query.pages[0].langlinks[?(@.lang == '%1$s')]", translationQuery.getToLanguage());
+        String wordURLPath = ".query.pages[0].fullurl";
+
+        String translationURI = MediaWikiAPIRequestBuilder.buildTranslationRequestURI(translationQuery.getWord(), translationQuery.getFromLanguage());
+        String APIResponse = JsoupRequestWrapper.tryToRetrieveJson(translationURI);
+
+        LinkedHashMap langAndLink = (LinkedHashMap) (((JSONArray) JsonPath.parse(APIResponse).read(langAndLinksPath)).get(0));
+        String translation = langAndLink.get("title").toString();
+        String translationUrl = langAndLink.get("url").toString();
+
+        String wordURL = ((JSONArray) JsonPath.parse(APIResponse).read(wordURLPath)).get(0).toString();
+
+        TranslationResponse translationResponse = new TranslationResponse();
+        translationResponse.setFromLanguage(translationQuery.getFromLanguage());
+        translationResponse.setToLanguage(translationQuery.getToLanguage());
+        translationResponse.setWord(translationQuery.getWord());
+        translationResponse.setWordURL(wordURL);
+        translationResponse.setWordWikiExtract(extractFirsSentenceFromArticle(wordURL));
+        translationResponse.setTranslation(translation);
+        translationResponse.setTranslationURL(translationUrl);
+        translationResponse.setTranslationWikiExtract(extractFirsSentenceFromArticle(translationUrl));
+
+        return translationResponse;
+    }
+
+    private String extractFirsSentenceFromArticle(String url) {
+        String firstParagraph = extractFirstParagraphFromArticle(url);
+
+        int endOfSentence;
+        for (endOfSentence = Math.min(120, firstParagraph.length()); endOfSentence < firstParagraph.length(); endOfSentence++) {
+            if (Character.isUpperCase(firstParagraph.charAt(endOfSentence))
+                    && firstParagraph.charAt(endOfSentence - 1) == ' '
+                    && firstParagraph.charAt(endOfSentence - 2) == '.') {
+                endOfSentence -= 2;
+                break;
+            }
+        }
+
+        return replaceAllIndexPointers(firstParagraph.substring(0, endOfSentence) + "...");
+    }
+
+    private String extractFirstParagraphFromArticle(String url) {
+        Document document = JsoupRequestWrapper.tryToRetrieveDocument(url);
+
+        return document.select("#mw-content-text > p").get(0).text();
+    }
+
+    private String replaceAllIndexPointers(String string) {
+        return string.replaceAll("\\[(.*?)\\]", "");
     }
 }
--- src/main/java/com/sensdu/utility/JsoupRequestWrapper.java
+++ src/main/java/com/sensdu/utility/JsoupRequestWrapper.java
@@ -1,8 +1,14 @@
 package com.sensdu.utility;
 
-import java.io.IOException;
-
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * According to the User-Agent policy -
@@ -13,15 +19,53 @@ import org.jsoup.Jsoup;
  */
 public class JsoupRequestWrapper {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsoupRequestWrapper.class);
+
+    private static final String USER_AGENT = "Sensdu/1.0";
+
+    private static final String REFERRER = "http://www.sensdu.com";
+
     private JsoupRequestWrapper() {
     }
 
-    public static String retrieveJson(String url) throws IOException {
+    public static Document tryToRetrieveDocument(String url) {
+        try {
+            String decodedURL = URLDecoder.decode(url, StandardCharsets.UTF_8.toString());
+
+            return retrieveDocument(decodedURL);
+        } catch (IOException exception) {
+            String message = "Can not parse " + url + " - " + exception.getMessage();
+            LOGGER.error(message);
+
+            throw new RuntimeException(message);
+        }
+    }
+
+    public static String tryToRetrieveJson(String url) {
+        try {
+            String decodedURL = URLDecoder.decode(url, StandardCharsets.UTF_8.toString());
+
+            return retrieveJson(decodedURL);
+        } catch (IOException exception) {
+            String message = "Can not connect to " + url + " - " + exception.getMessage();
+            LOGGER.error(message);
+
+            throw new RuntimeException(message);
+        }
+    }
+
+    private static Document retrieveDocument(String url) throws IOException {
+        return Jsoup.connect(url)
+                .userAgent(USER_AGENT)
+                .referrer(REFERRER)
+                .get();
+    }
+
+    private static String retrieveJson(String url) throws IOException {
         return Jsoup.connect(url)
                 .ignoreContentType(true)
-                .userAgent("Sensdu/1.0 (http://www.sensdu.com)")
-                .referrer("http://www.sensdu.com")
-                .timeout(12000)
+                .userAgent(USER_AGENT)
+                .referrer(REFERRER)
                 .execute()
                 .body();
     }
